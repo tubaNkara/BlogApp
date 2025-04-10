1844df3814f917942f70eb61323717dcaf198352
Bahar Yılmaz <ylmzbhr1@gmail.com>
2025-04-10 22:01:15 +0300

    Update user.model.js

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -27,17 +27,6 @@ const UserSchema = new mongoose.Schema({
         required: [true, 'Email field is required'],
         unique: [true, 'This email address is already in use.'],
         trim: true,
-        //* How validate works?
-        // validate: (email) => { return false }
-
-        //* Throw validation error
-        // validate: (email) => {
-        //     if (email.includes('@') && email.includes('.')) {
-        //         return true
-        //     }
-        //     throw new Error('invalid email address')
-        // },
-
         validate: [(email) => {
             return (email.includes('@') && email.includes('.'))
         }, 'Invalid email address']
